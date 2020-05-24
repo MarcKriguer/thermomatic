3c4c3c2317d0384bd358856c23ac65471aae991e
Marc Kriguer <Marc.Kriguer@gmail.com>
2020-05-23 20:40:42 -0700

    Implement imei logic and tests.

--- internal/imei/imei.go
+++ internal/imei/imei.go
@@ -7,24 +7,67 @@ package imei
 // https://en.wikipedia.org/wiki/International_Mobile_Equipment_Identity.
 
 import (
-	"errors"
-	"github.com/MarcKriguer/thermomatic/internal/common"
+  "errors"
+  "github.com/MarcKriguer/thermomatic/internal/common"
 )
 
 var (
-	ErrInvalid  = errors.New("imei: invalid ")
-	ErrChecksum = errors.New("imei: invalid checksum")
+  ErrChecksum = errors.New("imei: invalid IMEI checksum")
+  ErrImeiSize = errors.New("imei: invalid IMEI size (must be 15 characters)")
+  ErrInvalid  = errors.New("imei: invalid IMEI character(s)")
 )
 
-// Decode returns the IMEI code contained in the first 15 bytes of b.
+const IMEI_LENGTH = 15
+
+// Decode returns the IMEI code contained in b.
 //
-// In case b isn't strictly composed of digits, the returned error will be
-// ErrInvalid.
+// If b isn't exactly 15 bytes long, the returned error will be ErrImeiSize.
+//
+// In case b isn't strictly composed of digits, the returned error will be ErrInvalid.
 //
 // In case b's checksum is wrong, the returned error will be ErrChecksum.
 //
 // Decode does NOT allocate under any condition. Additionally, it panics if b
-// isn't at least 15 bytes long.
+// isn't exactly 15 bytes long.
 func Decode(b []byte) (code uint64, err error) {
-	panic(common.ErrNotImplemented)
+  // make sure length is correct
+  if (len(b) != IMEI_LENGTH) {
+    common.LogError(ErrImeiSize)
+    panic(ErrImeiSize)
+  }
+
+  // go through each byte of b -- build up checksum and code, byte by byte (return an error, but
+  // not panic, if either the checksum is wrong or an invalid byte is encountered).
+  var checksum uint8  = 0
+  var results  uint64 = 0
+  var digit    uint8
+
+  for i := 0; i < IMEI_LENGTH; i++ {
+    // make sure the byte represents a digit
+    digit = uint8(b[i])
+    if digit > 9 {
+      common.LogError(ErrInvalid)
+      return 0, ErrInvalid
+    }
+
+    results *= uint64(10)
+    results += uint64(digit)
+    // checksum adds the value of even-positioned digits,
+    // and double the value of odd-positioned digits (plus 1 if a 2-digit result)
+    if (i % 2 == 0) {
+      checksum += digit
+    } else {
+      checksum += (digit * 2)
+      if (digit > 4) {
+        checksum++
+      }
+    }
+  }
+
+  // checksum needs to end with a zero (after all bytes have been counted) to be valid
+  if checksum % 10 != 0 {
+    common.LogError(ErrChecksum)
+    return 0, ErrChecksum
+  }
+  return results, nil
 }
--- internal/imei/imei_test.go
+++ internal/imei/imei_test.go
@@ -1,22 +1,93 @@
 package imei
 
 import (
-	"github.com/MarcKriguer/thermomatic/internal/common"
+	"github.com/MarcKriguer/thermomatic/internal/imei"
 	"testing"
 )
 
 func TestDecode(t *testing.T) {
-	panic(common.ErrNotImplemented)
-}
+  // Happy path: make sure valid IMEI is decoded properly
+  var validimei = [...]byte { 4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8}
+  result, err := imei.Decode(validimei[:])
+  if err != nil {
+    t.Errorf("Calling Decode received unexpected error.")
+  }
 
-func TestDecodeAllocations(t *testing.T) {
-	panic(common.ErrNotImplemented)
+  // make sure it was decoded properly
+  if result != 490154203237518 {
+    t.Errorf("Received unexpected result")
+  }
 }
 
 func TestDecodePanics(t *testing.T) {
-	panic(common.ErrNotImplemented)
+  // unhappy path 1: invalid IMEI length
+  var invalidimei = [...]byte { 4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1}
+
+  defer func() {
+    if r := recover(); r == nil {
+      t.Errorf("Decode did not panic when expected")
+    }
+  }()
+
+  result, err := imei.Decode(invalidimei[:])
+  if (err != nil) {
+    t.Errorf("Decode returned an error instead of panicing.")
+  }
+  if (result != 0) {
+    t.Errorf("Decode returned a value instead of panicing.")
+  }
+}
+
+func TestDecodeBadChecksum(t *testing.T) {
+  // unhappy path 2: invalid IMEI checksum
+  var invalidimei = [...]byte { 4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 9}
+
+  defer func() {
+    if r := recover(); r != nil {
+      t.Errorf("Decode unexpectedly paniced")
+    }
+  }()
+
+  result, err := imei.Decode(invalidimei[:])
+  if err != imei.ErrChecksum {
+      t.Errorf("Decode received a different error than expected.");
+  }
+  if (result != 0) {
+    t.Errorf("Decode returned a value instead of an error.")
+  }
+}
+
+func TestDecodeBadCharacter(t *testing.T) {
+  // unhappy path 3: invalid IMEI character
+  var invalidimei = [...]byte { 52, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8}
+
+  defer func() {
+    if r := recover(); r != nil {
+      t.Errorf("Decode unexpectedly paniced")
+    }
+  }()
+
+  result, err := imei.Decode(invalidimei[:])
+  if err != imei.ErrInvalid {
+      t.Errorf("Decode received a different error than expected.");
+  }
+  if (result != 0) {
+    t.Errorf("Decode returned a value instead of an error.")
+  }
 }
 
 func BenchmarkDecode(b *testing.B) {
-	panic(common.ErrNotImplemented)
+  b.ReportAllocs()
+  var validimei = [...]byte { 4, 9, 0, 1, 5, 4, 2, 0, 3, 2, 3, 7, 5, 1, 8}
+  for i := 0; i < b.N; i++ {
+    result, err := imei.Decode(validimei[:])
+    if err != nil {
+      b.Error("Calling Decode received unexpected error in Benchmark.")
+    }
+
+    // make sure it was decoded properly
+    if result != 490154203237518 {
+      b.Error("Received unexpected result in Benchmark.")
+    }
+  }
 }
