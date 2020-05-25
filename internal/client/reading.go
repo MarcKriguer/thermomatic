d08c0a22eac137cf07eff863e4e36e20e33448f8
Marc Kriguer <Marc.Kriguer@gmail.com>
2020-05-24 23:28:40 -0700

    Finish Reading functionality (and add unit tests).

--- internal/client/reading.go
+++ internal/client/reading.go
@@ -1,31 +1,100 @@
 package client
 
-import "github.com/MarcKriguer/thermomatic/internal/common"
+import (
+  "bytes"
+  "encoding/binary"
+  "errors"
+  "fmt"
+  "github.com/MarcKriguer/thermomatic/internal/common"
+  "math"
+)
+
+var (
+  ErrReadingLength = errors.New("client: Reading byte array is less than 40 characters long.")
+)
+
+const READING_LENGTH = 40
 
 // Reading is the set of device readings.
 type Reading struct {
-	// Temperature denotes the temperature reading of the message.
+	// Temperature denotes the temperature reading of the message.  Valid range [-300, 300].
 	Temperature float64
 
-	// Altitude denotes the altitude reading of the message.
+	// Altitude denotes the altitude reading of the message.  Valid range [-20000, 20000].
 	Altitude float64
 
-	// Latitude denotes the latitude reading of the message.
+	// Latitude denotes the latitude reading of the message.  Valid range [-90, 90].
 	Latitude float64
 
-	// Longitude denotes the longitude reading of the message.
+	// Longitude denotes the longitude reading of the message.  Valid range [-180, 180].
 	Longitude float64
 
-	// BatteryLevel denotes the battery level reading of the message.
+	// BatteryLevel denotes the battery level reading of the message.  Valid range (0, 100].
 	BatteryLevel float64
 }
 
 // Decode decodes the reading message payload in the given b into r.
 //
-// If any of the fields are outside their valid min/max ranges ok will be unset.
+// Returns true if all fields are within min/max ranges, false if any are outside their range.
 //
-// Decode does NOT allocate under any condition. Additionally, it panics if b
-// isn't at least 40 bytes long.
+// Decode does NOT allocate under any condition.
+// Additionally, it panics if b isn't at least 40 bytes long.
 func (r *Reading) Decode(b []byte) (ok bool) {
-	panic(common.ErrNotImplemented)
+  // panic if byte array is too small
+  if len(b) < READING_LENGTH {
+    common.LogError(ErrReadingLength)
+    panic(ErrReadingLength)
+  }
+
+  // extract each field
+  r.Temperature  = math.Float64frombits(binary.BigEndian.Uint64(b[0:8]))
+  r.Altitude     = math.Float64frombits(binary.BigEndian.Uint64(b[8:16]))
+  r.Latitude     = math.Float64frombits(binary.BigEndian.Uint64(b[16:24]))
+  r.Longitude    = math.Float64frombits(binary.BigEndian.Uint64(b[24:32]))
+  r.BatteryLevel = math.Float64frombits(binary.BigEndian.Uint64(b[32:40]))
+
+  // validate each field, returning false if any of them are outside their range
+  if r.Temperature > 300 || r.Temperature < -300 {
+    common.LogOutput("Temperature out of range: " + fmt.Sprintf("%f", r.Temperature))
+    return false
+  }
+  if r.Altitude > 20000 || r.Altitude < -20000 {
+    common.LogOutput("Altitude out of range: " + fmt.Sprintf("%f", r.Altitude))
+    return false
+  }
+  if r.Latitude > 90 || r.Latitude < -90 {
+    common.LogOutput("Latitude out of range: " + fmt.Sprintf("%f", r.Latitude))
+    return false
+  }
+  if r.Longitude > 180 || r.Longitude < -180 {
+    common.LogOutput("Longitude out of range: " + fmt.Sprintf("%f", r.Longitude))
+    return false
+  }
+  if r.BatteryLevel > 100 || r.BatteryLevel <= 0 {
+    common.LogOutput("BatteryLevel out of range: " + fmt.Sprintf("%f", r.BatteryLevel))
+    return false
+  }
+
+  return true
+}
+
+// Encode encodes the reading message payload in the given r into a byte array.
+func (r *Reading) Encode() (buf []byte) {
+  var buffer []byte
+  buffer = append(buffer, Float64ToByteArray(r.Temperature)...)
+  buffer = append(buffer, Float64ToByteArray(r.Altitude)...)
+  buffer = append(buffer, Float64ToByteArray(r.Latitude)...)
+  buffer = append(buffer, Float64ToByteArray(r.Longitude)...)
+  buffer = append(buffer, Float64ToByteArray(r.BatteryLevel)...)
+  return buffer
+}
+
+// Generate an 8-byte array containing a 64-bit float
+func Float64ToByteArray(field float64) (buf []byte) {
+  buffer := new (bytes.Buffer)
+  err := binary.Write(buffer, binary.BigEndian, field)
+  if err != nil { 
+    common.LogError(err)
+  }
+  return buffer.Bytes()
 }
---
+++ internal/client/reading_test.go
@@ -0,0 +1,202 @@
+package client
+
+import (
+  "bytes"
+  "testing"
+)
+
+// Test the Decode function, when exactly 40 bytes are passed in.
+func TestReadingDecodeExact40(t *testing.T) {
+  var reading Reading
+
+  // Declare byte array to Decode
+  byteArray := []uint8{
+    0x40, 0x50, 0xf1, 0x47, 0xae, 0x14, 0x7a, 0xe1, // temperature
+    0x40, 0x05, 0x15, 0x9b, 0x3d, 0x07, 0xc8, 0x4b, // altitude
+    0x40, 0x40, 0xb4, 0x7a, 0xe1, 0x47, 0xae, 0x14, // latitude
+    0x40, 0x46, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, // longitude
+    0x3f, 0xd0, 0x6d, 0x1e, 0x10, 0x8c, 0x3f, 0x3e, // battery level
+  }
+
+  result := reading.Decode(byteArray)
+  if !result {
+    t.Errorf("Failed to decode byte array")
+  }
+
+  if reading.Temperature != 67.77 {
+    t.Errorf("Error decoding Temperature (found %x)", reading.Temperature)
+  }
+  if reading.Altitude != 2.63555 {
+    t.Errorf("Error decoding Altitude (found %x)", reading.Altitude)
+  }
+  if reading.Latitude != 33.41 {
+    t.Errorf("Error decoding Latitude (found %x)", reading.Latitude)
+  }
+  if reading.Longitude != 44.4 {
+    t.Errorf("Error decoding Longitude (found %x)", reading.Longitude)
+  }
+  if reading.BatteryLevel != 0.25666 {
+    t.Errorf("Error decoding BatteryLevel (found %x)", reading.BatteryLevel)
+  }
+}
+
+// Test the Decode function, when more than 40 bytes are passed in.
+func TestReadingDecodeOver40(t *testing.T) {
+  var reading Reading
+
+  // Declare byte array to Decode
+  byteArray := []uint8{
+    0x40, 0x50, 0xf1, 0x47, 0xae, 0x14, 0x7a, 0xe1, // temperature
+    0x40, 0x05, 0x15, 0x9b, 0x3d, 0x07, 0xc8, 0x4b, // altitude
+    0x40, 0x40, 0xb4, 0x7a, 0xe1, 0x47, 0xae, 0x14, // latitude
+    0x40, 0x46, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, // longitude
+    0x3f, 0xd0, 0x6d, 0x1e, 0x10, 0x8c, 0x3f, 0x3e, // battery level
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ignored
+  }
+
+  result := reading.Decode(byteArray)
+  if !result {
+    t.Errorf("Failed to decode larger byte array")
+  }
+
+  if reading.Temperature != 67.77 {
+    t.Errorf("Error decoding Temperature (found %x)", reading.Temperature)
+  }
+  if reading.Altitude != 2.63555 {
+    t.Errorf("Error decoding Altitude (found %x)", reading.Altitude)
+  }
+  if reading.Latitude != 33.41 {
+    t.Errorf("Error decoding Latitude (found %x)", reading.Latitude)
+  }
+  if reading.Longitude != 44.4 {
+    t.Errorf("Error decoding Longitude (found %x)", reading.Longitude)
+  }
+  if reading.BatteryLevel != 0.25666 {
+    t.Errorf("Error decoding BatteryLevel (found %x)", reading.BatteryLevel)
+  }
+}
+
+// Test that the Decode function panics when less than 40 bytes are passed in.
+func TestReadingDecodeUnder40(t *testing.T) {
+  var reading Reading
+
+  // Declare byte array to Decode
+  byteArray := []uint8{
+    0x40, 0x50, 0xf1, 0x47, 0xae, 0x14, 0x7a, 0xe1, // temperature
+    0x40, 0x05, 0x15, 0x9b, 0x3d, 0x07, 0xc8, 0x4b, // altitude
+    0x40, 0x40, 0xb4, 0x7a, 0xe1, 0x47, 0xae, 0x14, // latitude
+    0x40, 0x46, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, // longitude
+    0x3f, 0xd0, 0x6d, 0x1e, 0x10, 0x8c, 0x3f,       // battery level (missing final byte)
+  }
+
+  defer func() {
+   if r := recover(); r == nil {
+      t.Errorf("Reading.Decode did not panic when expected")
+    }
+  }()
+
+  result := reading.Decode(byteArray)
+  if !result {
+    t.Errorf("Error (but not expected panic) when calling Reading.Decode with too few bytes.")
+  }
+}
+
+// Test that the Decode function returns false (but doesn't panic) when a field is out of range
+func TestReadingDecodeWithInvalidBatteryLevel(t *testing.T) {
+  var reading Reading
+
+  // Declare byte array to Decode
+  byteArray := []uint8{
+    0x40, 0x50, 0xf1, 0x47, 0xae, 0x14, 0x7a, 0xe1, // temperature
+    0x40, 0x05, 0x15, 0x9b, 0x3d, 0x07, 0xc8, 0x4b, // altitude
+    0x40, 0x40, 0xb4, 0x7a, 0xe1, 0x47, 0xae, 0x14, // latitude
+    0x40, 0x46, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, // longitude
+    0xc0, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // invalid BatteryLevel of -5
+  }
+
+  defer func() {
+   if r := recover(); r != nil {
+      t.Errorf("Reading.Decode panicked when it just should have returned false")
+    }
+  }()
+
+  result := reading.Decode(byteArray)
+  if result {
+    t.Errorf("Did not get a false (error) result when calling Reading.Decode with invalid data.")
+  }
+}
+
+// This tests the Encode function (used by the client to test the server).
+func TestReadingEncode(t *testing.T) {
+  // Declare a Reading struc and stuff in the values from the provided assignment's format example..
+  var reading Reading
+  reading.Temperature = 67.77
+  reading.Altitude = 2.63555
+  reading.Latitude = 33.41
+  reading.Longitude = 44.4
+  reading.BatteryLevel = 0.25666
+
+  // Encode to get the byte array
+  encoded := reading.Encode()
+
+  // Declare byte arrays for comparison
+  temperature := []uint8{0x40, 0x50, 0xf1, 0x47, 0xae, 0x14, 0x7a, 0xe1}
+  altitude    := []uint8{0x40, 0x05, 0x15, 0x9b, 0x3d, 0x07, 0xc8, 0x4b}
+  latitude    := []uint8{0x40, 0x40, 0xb4, 0x7a, 0xe1, 0x47, 0xae, 0x14}
+  longitude   := []uint8{0x40, 0x46, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33}
+  battery     := []uint8{0x3f, 0xd0, 0x6d, 0x1e, 0x10, 0x8c, 0x3f, 0x3e}
+
+  // Compare each element of the Reading with the expected Byte arrays
+  if !bytes.Equal([]byte(temperature), []byte(encoded[0:8])) {
+    t.Errorf("Error in encoding Temperature (was %x)", encoded[0:8])
+  }
+  if !bytes.Equal([]byte(altitude), []byte(encoded[8:16])) {
+    t.Errorf("Error in encoding Altitude (was %x)", encoded[8:16])
+  }
+  if !bytes.Equal([]byte(latitude), []byte(encoded[16:24])) {
+    t.Errorf("Error in encoding Latitude (was %x)", encoded[16:24])
+  }
+  if !bytes.Equal([]byte(longitude), []byte(encoded[24:32])) {
+    t.Errorf("Error in encoding Longitude (was %x)", encoded[24:32])
+  }
+  if !bytes.Equal([]byte(battery), []byte(encoded[32:40])) {
+    t.Errorf("Error in encoding BatteryLevel (was %x)", encoded[32:40])
+  }
+}
+
+func BenchmarkReadingDecode(b *testing.B) {
+  b.ReportAllocs()
+  var reading Reading
+
+  // Declare byte array to Decode
+  byteArray := []uint8{
+    0x40, 0x50, 0xf1, 0x47, 0xae, 0x14, 0x7a, 0xe1, // temperature
+    0x40, 0x05, 0x15, 0x9b, 0x3d, 0x07, 0xc8, 0x4b, // altitude
+    0x40, 0x40, 0xb4, 0x7a, 0xe1, 0x47, 0xae, 0x14, // latitude
+    0x40, 0x46, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, // longitude
+    0x3f, 0xd0, 0x6d, 0x1e, 0x10, 0x8c, 0x3f, 0x3e, // battery level
+  }
+
+  for i := 0; i < b.N; i++ {
+    result := reading.Decode(byteArray)
+    if !result {
+      b.Errorf("Failed to decode byte array in Benchmark.")
+    }
+  
+    if reading.Temperature != 67.77 {
+      b.Errorf("Error decoding Temperature (found %x)", reading.Temperature)
+    }
+    if reading.Altitude != 2.63555 {
+      b.Errorf("Error decoding Altitude (found %x)", reading.Altitude)
+    }
+    if reading.Latitude != 33.41 {
+      b.Errorf("Error decoding Latitude (found %x)", reading.Latitude)
+    }
+    if reading.Longitude != 44.4 {
+      b.Errorf("Error decoding Longitude (found %x)", reading.Longitude)
+    }
+    if reading.BatteryLevel != 0.25666 {
+      b.Errorf("Error decoding BatteryLevel (found %x)", reading.BatteryLevel)
+    }
+  }
+}
